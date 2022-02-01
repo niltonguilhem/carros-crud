eff6bfce7438cfc58e4aa900f699a779a1c4e2f1
Nilton Guilhem <nil.guilhem@gmail.com>
2022-02-01 10:02:10 -0300

    Criação da CarrosControlles e  Carro Service

--- src/main/java/com/example/carros/CarroApplication.java
+++ src/main/java/com/example/carros/CarroApplication.java
@@ -1,4 +1,4 @@
-package com.example.carro;
+package com.example.carros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/carros/CarroApplicationTests.java
+++ src/test/java/com/example/carros/CarroApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.carro;
+package com.example.carros;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
