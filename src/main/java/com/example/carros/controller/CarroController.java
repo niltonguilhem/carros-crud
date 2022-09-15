bf5f7e81221e9bf21d08399fa14157d2950da458
niltonguiilhem <nil.guilhem@gmail.com>
2022-09-15 12:39:31 -0300

    Update Logging

--- src/main/java/com/example/carros/controller/CarroController.java
+++ src/main/java/com/example/carros/controller/CarroController.java
@@ -4,6 +4,8 @@ import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroRequest;
 import com.example.carros.domain.CarroResponse;
 import com.example.carros.service.CarroService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,49 +18,51 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping ("/api/v1/carros")
 public class CarroController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CarroController.class);
     @Autowired
     private CarroService service;
 
     @GetMapping()
     public ResponseEntity<List<CarroResponse>> getAllCarro() {
+        logger.info("m=getAllCarro - status=start");
         List<Carro> carroList = service.findAllCarro();
         List<CarroResponse> carroResponseList = carroList.stream().map(carro -> new CarroResponse()
                 .withBuilderId(carro.getId())
                 .withBuilderNome(carro.getNome())
                 .withBuilderTipo(carro.getTipo())).toList();
+        logger.info("m=getAllCarro - status=finish");
         return new ResponseEntity<>(carroResponseList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CarroResponse> getId(@PathVariable("id") Long id) {
+    public ResponseEntity<CarroResponse> getIdCarro(@PathVariable("id") Long id) {
+        logger.info("m=getIdCarro - status=start " + id);
         Carro carro = service.getCarroById(id);
         CarroResponse response = new CarroResponse()
                 .withBuilderId(carro.getId())
                 .withBuilderNome(carro.getNome())
                 .withBuilderTipo(carro.getTipo());
-
+        logger.info("m=getIdCarro - status=finish " + id);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
 
-    /*@GetMapping("/tipo/{tipo}")
-    public Iterable<Carro> getTipo (@PathVariable ("tipo") String tipo ) {
-        return service.getCarroByTipo(tipo);
-    }*/
     @GetMapping("/tipo/{tipo}")
     public ResponseEntity<List<CarroResponse>> getTipo(@PathVariable("tipo") String tipo) {
+        logger.info("m=getTipo - status=start " + tipo);
         List<Carro> carroList = service.getCarroByTipo(tipo);
         List<CarroResponse> response = carroList.stream().map(carro -> new CarroResponse()
                 .withBuilderId(carro.getId())
                 .withBuilderNome(carro.getNome())
                 .withBuilderTipo(carro.getTipo())).collect(Collectors.toList());
-
+        logger.info("m=getTipo - status=finish " + tipo);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<CarroResponse> postCarro(@RequestBody CarroRequest carroRequest){
-
+        logger.info("m=postCarro - status=start");
         Carro carro = service.save(new Carro()
                 .withBuilderNome(carroRequest.getNome())
                 .withBuilderTipo(carroRequest.getTipo()));
@@ -67,14 +71,14 @@ public class CarroController {
                 .withBuilderId(carro.getId())
                 .withBuilderNome(carro.getNome())
                 .withBuilderTipo(carro.getTipo());
-
+        logger.info("m=postCarro - status=finish");
         return new ResponseEntity<>(response,HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CarroResponse> putCarro (@PathVariable("id")Long id,
                                                        @RequestBody CarroRequest carroRequest){
-
+        logger.info("m=putCarro - status=start " + id);
         Carro carroUpdate = service.update(new Carro()
                 .withBuilderId(id)
                 .withBuilderNome(carroRequest.getNome())
@@ -84,17 +88,15 @@ public class CarroController {
                 .withBuilderId(carroUpdate.getId())
                 .withBuilderNome(carroUpdate.getNome())
                 .withBuilderTipo(carroUpdate.getTipo());
-
-
+        logger.info("m=putCarro - status=finish " + id);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public  String delete(@PathVariable("id") Long id) {
-
+    public void delete(@PathVariable("id") Long id) {
+        logger.info("m=delete - status=start " + id);
         service.delete(id);
-
-        return "Carro deletado com sucesso";
+        logger.info("m=delete - status=finish " + id);
     }
 
 
--- src/main/java/com/example/carros/service/CarroService.java
+++ src/main/java/com/example/carros/service/CarroService.java
@@ -2,6 +2,8 @@ package com.example.carros.service;
 
 import com.example.carros.domain.Carro;
 import com.example.carros.repository.CarroRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -13,45 +15,69 @@ import java.util.Optional;
 @Service
 public class CarroService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CarroService.class);
+
     @Autowired
     private CarroRepository repository;
 
-    public List<Carro> findAllCarro() { return repository.findAll();}
+    public List<Carro> findAllCarro() {
+        logger.info("m=findAllCarro - status=start");
+        List<Carro> carroList = repository.findAll();
+        logger.info("m=findAllCarro - status= finish");
+        return carroList;}
 
-    public Carro getCarroById(Long id){return repository.findById(id).get();}
+    public Carro getCarroById(Long id){
+        logger.info("m=getCarroById - status=start " + id);
+        Carro carro = repository.findById(id).get();
+        logger.info("m=getCarroById - status=finish " + id);
+        return carro;
+    }
     public Optional<Carro> getCarroByIdOptional(Long id) {
-        return  repository.findById(id);
+        logger.info("m=getCarroByIdOptional - status=start " + id);
+        Optional<Carro> carroOptional = repository.findById(id);
+        logger.info("m=getCarroByIdOptional - status=finish " + id);
+        return  carroOptional;
     }
 
     public List<Carro> getCarroByTipo (String tipo) {
-        return repository.findByTipo(tipo);
+        logger.info("m=getCarroByTipo - status=start " + tipo);
+        List<Carro> carroList = repository.findByTipo(tipo);
+        logger.info("m=getCarroByTipo - status=finish " + tipo);
+        return carroList;
     }
 
     public Carro save (Carro carro) {
+        logger.info("m=save - status=start");
         Assert.isNull(carro.getId(), "Não foi possível inserir o registro");
+        logger.info("m=save - status=finish");
         return repository.save(carro);
     }
 
     public Carro update(Carro carro) {
-
+        logger.info("m=update - status=start " + carro.getId());
         Optional<Carro> optional = getCarroByIdOptional(carro.getId());
         if (optional.isPresent()) {
             Carro carroEntity = carro;
             carroEntity.setNome(carro.getNome());
             carroEntity.setTipo(carro.getTipo());
-
             repository.save(carroEntity);
-
+            logger.info("m=update - status=finish " + carro.getId());
             return carroEntity;
         } else {
-            throw new RuntimeException();
+            logger.warn("m=update - status=warn " + carro.getId());
+            throw new RuntimeException("O id informado é inexistente.");
         }
     }
 
     public void delete(Long id) {
+        logger.info("m=delete - status=start " + id);
         Optional<Carro> carro = getCarroByIdOptional(id);
         if(carro.isPresent()) {
+            logger.info("m=delete - status=finish " + id);
             repository.deleteById(id);
+        } else {
+            logger.warn("m=delete - status=warn " + id);
+            throw new RuntimeException("O id informado é inexistente.");
         }
     }
 
